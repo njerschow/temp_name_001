85b6ea305cb52d108efd315e2a57a8aef57fe11c
njerschow <nj18@nyu.edu>
2022-07-26 13:58:52 -0400

    add redirect to app page

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Footer from '../src/partials/Footer'
 import Hero from '../src/partials/Hero'
 
+export function getServerSideProps (context) {
+  // redirect to app
+  context.res.writeHead(301, {
+    Location: '/app'
+  })
+  context.res.end()
+}
+
 function Home () {
   return (
     <div className='flex flex-col min-h-screen overflow-hidden'>
